9a613176c612293e53d0a6d2405638530feef2c5
FABIO DOS SANTOS GONCALVES <33531106821@cmdo.policiamilitar.sp.gov.br>
2018-06-06 13:34:19 -0300

    Primeiro comite com o projeto

---
+++ FilaMSMQ.ColocaNaFila/Program.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilaMSMQ.ColocaNaFila
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomeFila = @".\private$\MinhaPrimeiraFila";
+            MessageQueue queue = new MessageQueue();
+            queue.Path = nomeFila;
+            Message message = new Message();
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("Digite o Nome");
+                var nomeDigitado = Console.ReadLine();
+                Console.WriteLine("Digite a Idade");
+                var idadeDigitada = Console.ReadLine();
+                message.Body = JsonConvert.SerializeObject(new Modelo() { Nome = nomeDigitado, Idade = int.Parse(idadeDigitada) });
+                queue.Send(message);
+            }
+            
+            Console.WriteLine("FIM");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ FilaMSMQ.LerDaFila/Program.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilaMSMQ.LerDaFila
+{
+    public class Program
+    {
+        private static MessageQueue _queue;
+
+
+        static void Main(string[] args)
+        {
+            string nomeFila = @".\private$\MinhaPrimeiraFila";
+
+            _queue = new MessageQueue();
+            _queue.Path = nomeFila;
+            _queue.ReceiveCompleted += new ReceiveCompletedEventHandler(ProcessaFila);
+            _queue.Formatter = new XmlMessageFormatter(new[] { typeof(string) });
+            _queue.BeginReceive();
+            Console.WriteLine("Tecle algo para sair");
+            Console.ReadKey();
+        }
+
+        private static void ProcessaFila(object sender, ReceiveCompletedEventArgs e)
+        {
+            _queue.EndReceive(e.AsyncResult);
+            object itemDaFila = e.Message.Body;
+            var modelo = JsonConvert.DeserializeObject<Modelo>(itemDaFila.ToString());
+            Console.WriteLine($"O Nome e {modelo.Nome} , idade {modelo.Idade}");
+            _queue.BeginReceive();//Vai buscar outro item da fila
+        }
+    }
+}
